8bd29351140778b89b3d069224e9216b9d2dd66a
ParadoxFlame <shivammaji5818@gmail.com>
2021-04-04 17:57:14 -0700

    4/4/21

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
 var database;
 var back_img;
-var gameState =0;
+var gameState = 0;
 var playerCount = 0;
 var allPlayers;
-var score =0;
-var player, form,game;
-var player1,player2;
+var score = 0;
+var player, form, game;
+var player1, player2;
 var players;
 var fruits;
 var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
-var player1score =0;
-var player2score =0;
+var player1score = 0;
+var player2score = 0;
 
-function preload(){
+function preload() {
+  //loaading all the images
   back_img = loadImage("images/jungle.jpg");
   player_img = loadImage("images/basket2.png");
   fruit1_img = loadImage("images/apple2.png");
@@ -25,26 +26,28 @@ function preload(){
   fruitGroup = new Group();
 }
 function setup() {
+  //create a new gameobject, fetch gamestate from Db, start the game
   createCanvas(1000, 600);
   database = firebase.database();
   game = new Game();
   game.getState();
   game.start();
-  
+
 }
 
 function draw() {
+  //playing with different gamestatesm DB
   background(back_img);
-  
-   if (playerCount === 2) {
-     game.update(1);
-   }
-   if (gameState === 1) {
-     clear(); 
-     game.play();
-   }
-   if (gameState === 2) {
-    
-     game.end();
-   }
+
+  if (playerCount === 2) {
+    game.update(1);
+  }
+  if (gameState === 1) {
+    clear();
+    game.play();
+  }
+  if (gameState === 2) {
+
+    game.end();
+  }
 }
